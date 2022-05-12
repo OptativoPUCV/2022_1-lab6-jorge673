67d1467442eb80f9da785bc450a683686b3fb297
Your Name <you@example.com>
2022-05-12 19:27:15 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -51,12 +51,13 @@ int is_valid(Node* n){
   int numero,k,p;
   int contS=0,contH=0,contV=0;
   
-  for(numero=1;numero<10;numero++){
-    for(k=0;k<9;k++){
+  for(k=0;k<9;k++){
+    for(numero=1;numero<10;numero++){
       for(p=0;p<9;p++){
         int i=3*(k/3) + (p/3) ;
         int j=3*(k%3) + (p%3) ;
         if(n->sudo[i][j]==numero)contS++;
+
       }
       for(p=0;p<9;p++){
         if(n->sudo[p][k]==numero)contV++;
