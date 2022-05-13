6c241988e7f179471f2efcb5fc9cbc21c78939d8
Your Name <you@example.com>
2022-05-12 20:03:32 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -115,6 +115,19 @@ int is_final(Node* n){
 }
 
 Node* DFS(Node* initial, int* cont){
+  Stack* S=createStack();
+  push(S,initial);
+  while (S){
+    Node* n=pop(S);
+    if(is_final(n))return n;
+    List* adj=get_adj_nodes(n);
+    Node* aux=first(adj);
+    while (aux){
+      push(S,aux);
+      aux=next(adj);
+    }
+    
+  }
   return NULL;
 }
 
